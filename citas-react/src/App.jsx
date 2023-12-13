1f014897d0402531fd94cf7af78fcd7c6bdc8b0a
Javier Fernandez <javier.fernandez@bulevip.com>
2023-12-13 22:35:26 +0100

    Solve Localstorag Error

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -1,18 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
-  const [pacientes, setPacientes] = useState([]);
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? []);
   const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    //carga cuando el componente este preparado
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLS);
+    };
+    obtenerLS();
+  }, []);
+
+  useEffect(() => {
+    //aqui usaremos el localstorage
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes]);
 
   const eliminarPaciente = (id) => {
     //me retorna una nuevo array con los que cumplen la condicion indicada, los que no tienen ese id
     const pacientesActualizados = pacientes.filter(
       (paciente) => paciente.id !== id
     );
-   setPacientes(pacientesActualizados)
+    setPacientes(pacientesActualizados);
   };
 
   return (
